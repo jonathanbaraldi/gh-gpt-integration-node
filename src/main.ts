50e57a9711fa707c192b43c1cc92082f68cc44d6
Adran Farias Carnavale <adran.carnavale@gmail.com>
2023-01-29 22:55:44 -0300

    main.ts

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Configuration, OpenAIApi } from 'openai'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const prompt = `explain and give suggestions about this code:
+function sum (a, b) {
+  return a - b
+}
+`;
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+openai.createCompletion({
+  model: "text-davinci-003",
+  prompt,
+  max_tokens: 2048,
+})
+.then((res) => console.log(res.data.choices[0].text));
